9a1885289ebc85d654a74cc10446b97ca7ed6741
Wlasov <93740526+Wlasov@users.noreply.github.com>
2023-03-15 10:04:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,106 +1,150 @@
 #include <iostream>
 
+// Цвет клетки и фигуры
 enum EColor
 {
-	Black = 0,
-	White
+    Black = 0,
+    White,
 };
 
+// Перерводит енум цвета в белый или черный квадратик
+std::string ColorToString(EColor color)
+{
+    switch (color)
+    {
+        case 0:
+            return "\u25A0 ";
+        case 1:
+            return  "\u25A1 ";
+        default:
+            return "  ";
+    }
+}
+
+// позиция, позволяет линейный массив клеток представить в виде 2мерного массива
 struct Position
 {
-	int col, row;
-	
-	Position(int col = 0, int row = 0)
-	{
-		this->col = col;
-		this->row = row;
-	}
+    uint16_t col, row;
+    
+    Position(uint16_t col, uint16_t row) : col(col), row(row) {}
 };
 
+// базовый класс фигуры
 class Piece
 {
 public:
-	Piece(const EColor color, const Position position)
-	{
-		_color = color;
-		_currentPosition = position;
-	}
-
-	virtual void Move(const Position& nextPosition) = 0;
-	virtual void Eat(const Piece& target) = 0;
-	virtual ~Piece() {}
-	
+    virtual void Move(const Position& nextPosition) = 0;
+    virtual bool IsMoveValiable(const Position& nextPosition) = 0;
+    virtual void Eat(const Piece& target) = 0;
+    virtual ~Piece() {};
+    
 private:
-	EColor _color;
-	Position _currentPosition;
+    EColor _color;
 };
 
 class Cell
 {
 public:
-	Cell(const EColor color = Black)
-		: _color(color)	
-	{}
-	~Cell(){}
-	
-	Cell(const Cell& cell)
-	  : _color(cell._color)
-	  , _currentPiece(cell._currentPiece)
-	{}
-	
-	Cell& operator= (const Cell& cell)
-	{
-		_color = cell._color;
-		_currentPiece = cell._currentPiece; 
-		return *this;
-	} 
-	
-	void PlaceFigure(Piece* piece)
-	{
-		_currentPiece = piece;
-	}
-	
-	EColor GetColor()
-	{
-		return _color;
-	}
-	
-	const Piece& GetPiece()
-	{
-		return *_currentPiece;
-	}
-		
+    Cell(){}
+    
+    Cell(EColor color, Position* position, Piece* piece = NULL) 
+    : _color(color)
+    , _currentPosition(position)
+    , _currentPiece(piece){}
+    
+    Cell(const Cell& cell)
+    {
+        _currentPiece = cell._currentPiece;
+        _color = cell._color;
+        _currentPosition = cell._currentPosition;
+    }
+    
+    Cell& operator=(const Cell& other)
+    {
+        _currentPiece = other._currentPiece;
+        _color = other._color;
+        _currentPosition = other._currentPosition;
+        return *this;
+    }
+    
+    ~Cell(){}
+    
+    EColor GetColor()
+    {
+        return _color;
+    }
+    
+    const Position& GetPosition()
+    {
+        return *_currentPosition;
+    }
+    
 private:
-	EColor _color;
-	Piece* _currentPiece;
+    Piece* _currentPiece;
+    EColor _color;
+    Position* _currentPosition;
 };
 
 class Board
 {
 public:
-	Board(const size_t size)
-	{
-		_size = size;
-		
-		_cells = new Cell[size*size];
-		
-		for (int i = 0; i < size; ++i)
-		{
-			for (int j = 0; j < size; ++j)
-			{
-				_cells[i+j] = new Cell();
-			}
-		}
-	}
-	
+    Board(size_t size)
+    {
+        _size = size;
+        _cells = new Cell[size*size];
+        int currentCell = 0;
+        
+        for (int i = 0; i < size; ++i)
+        {
+            for (int j = 0; j < size; ++j)
+            {
+                _cells[currentCell] = Cell( (i+j) % 2 ? Black : White , new Position(i, j));
+                ++currentCell;
+            }
+        }
+    }
+    
+    ~Board();
+    
+    void Print()
+    {
+        int currentCell = 0;
+        
+        for (int i = 0; i < _size; ++i)
+        {
+            std::cout<< _size - i << ' ';
+            
+            for (int i = 0; i < _size; ++i)
+            {
+                std::cout<<ColorToString(_cells[currentCell].GetColor());
+                ++currentCell;
+            }
+            
+            std::cout<<'\n';
+        }
+        
+        std::cout << "  ";
+        
+        for (int i = 0 ; i < _size; ++i)
+        {
+            std::cout << (char)(65+i) << ' ';
+        }
+    }
+    
+    
 private:
-	size_t _size;
-	Cell* _cells;
+    size_t _size;
+    Cell* _cells;
 };
 
-int main()
+int main(void)
 {
-	Board* board = new Board(8);
-
-	return 0;
+    Board* board8 = new Board(8);
+    board8->Print();
+    
+    std::cout<<"\n\n\n";
+    
+     Board* board12 = new Board(12);
+     board12->Print();
+    
 }
