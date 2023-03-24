fa01b59351acb92a744828f73de0e6bef4353d0a
Wlasov <93740526+Wlasov@users.noreply.github.com>
2023-03-24 23:20:59 +0300

    Update GameMenager.h

--- GameMenager.h
+++ GameMenager.h
@@ -8,8 +8,8 @@ class GameMenager()
 public:
     virtual void Start() = 0; // start game
     virtual void End() = 0; // end game
-    virtual void Surrender(Player* sender) = 0; // surrent the game
-    virtual void MakeMove(Player* sender) = 0; // make a move on the board
+    virtual void Surrender(Player* sender) = 0; // surrend the game
+    virtual void Move(Player* sender) = 0; // make a move on the board
     virtual void OnBoardDraw() = 0; // Draw board in console
 	
 private:
