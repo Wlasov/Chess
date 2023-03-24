4b5a0fd46f7df1cb6ba22c6191d446ea856a0889
Wlasov <93740526+Wlasov@users.noreply.github.com>
2023-03-24 19:26:46 +0300

    Update Coordinate.h

--- Coordinate.h
+++ Coordinate.h
@@ -4,14 +4,14 @@
 class Coordinate
 {
 public:
-	Coordinate();
-	Coordinate(size_t col, size_t row);	
-	~Coordinate();
-	
-	const size_t& GetColumn();
-	const size_t& GetRow();
+    Coordinate();
+    Coordinate(size_t col, size_t row);	
+    ~Coordinate();
+    
+    const size_t& GetColumn();
+    const size_t& GetRow();
 
 private:
-	size_t _col;
-	size_t _row;
+    size_t _col;
+    size_t _row;
 };
