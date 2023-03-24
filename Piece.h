ca130f8ccada9ec46867e874ec042bd296762604
Wlasov <93740526+Wlasov@users.noreply.github.com>
2023-03-24 19:30:15 +0300

    Update Piece.h

--- Piece.h
+++ Piece.h
@@ -6,11 +6,11 @@
 class Piece
 {
 public:
-	Piece();
-	Piece(EColor color);
-	virtual ~Piece(){};
+    Piece();
+    Piece(EColor color);
+    virtual ~Piece(){};
 	
-	virtual void Move(const Coordinate& nextPosition) = 0;
+    virtual void Move(const Coordinate& nextPosition) = 0;
 	
 private:
 	EColor _color;
