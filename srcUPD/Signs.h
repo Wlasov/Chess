4d6c9b08ae00292867509bb63cffc7779adda9e7
Wlasov <93740526+Wlasov@users.noreply.github.com>
2023-05-22 20:48:45 +0300

    Add files via upload

---
+++ srcUPD/Signs.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Cell's color
+#define BLACK_SQUARE_SIGN "\u25A0"
+#define WHITE_SQUARE_SIGN "\u25A1"
+
+// Piece's color
+#define WHITE_PAWN_SIGN "\u265F"
+#define BLACK_PAWN_SIGN "\u2659 "
+
+#define WHITE_QUEEN_SIGN "\u2655 "
+#define BLACK_QUEEN_SIGN "\u265B "
+
+#define WHITE_KING_SIGN "\u2654 "
+#define BLACK_KING_SIGN "\u265A "
+
+#define WHITE_ROOK_SIGN "\u2656 "
+#define BLACK_ROOK_SIGN "\u265C "
+
+#define WHITE_BISHOP_SIGN "\u2657 "
+#define BLACK_BISHOP_SIGN "\u265D "
+
+#define WHITE_KNIGHT_SIGN "\u2658 "
+#define BLACK_KNIGHT_SIGN "\u265E "
