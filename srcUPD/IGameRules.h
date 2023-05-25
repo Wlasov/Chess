3dcf2cb97098878c7c8fc4861964364217dcc7b6
Nikolay Vlasov <93740526+Wlasov@users.noreply.github.com>
2023-05-25 19:52:38 +0300

    Update IGameRules.h

--- srcUPD/IGameRules.h
+++ srcUPD/IGameRules.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Board.h"
 
+// Interface of game type
 class IGameRules
 {
 public:
-	virtual void MovePiece(const std::string& from, const std::string& to) = 0;
+	virtual void MovePiece(const std::string& from, const std::string& to) = 0; // move piece from "from" to "to".
 };
