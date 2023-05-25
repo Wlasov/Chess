5ba06fe514f22a6ba5f3328dc42ee145b9635f2f
Nikolay Vlasov <93740526+Wlasov@users.noreply.github.com>
2023-05-25 19:51:21 +0300

    Update Coordinate.h

--- srcUPD/Coordinate.h
+++ srcUPD/Coordinate.h
@@ -12,6 +12,12 @@ public:
 	size_t& GetRow();
 
 private:
-	size_t _col;
-	size_t _row;
-};
+	size_t _col; // board's column
+	size_t _row; // board's row
+}; 
+
+/*
+	Coordinate(0, 0) means A1,
+	Coordinate(1, 1) means B2,
+	Coordinate(7, 7) means H8,
+*/
