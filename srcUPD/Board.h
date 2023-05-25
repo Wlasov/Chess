e3666d749fde811f9c784254a4c76df610847d2c
Nikolay Vlasov <93740526+Wlasov@users.noreply.github.com>
2023-05-25 19:45:28 +0300

    Update Board.h

--- srcUPD/Board.h
+++ srcUPD/Board.h
@@ -8,16 +8,16 @@ public:
 	Board(size_t size);
 	~Board(){};
 	friend std::ostream& operator << (std::ostream& os, const Board& board);
-	void DebugPrint();
-	void DebugPrintPlayerPerspective();
-	void PlacePiece(Coordinate* coord, BasePiece* piece);
+	void DebugPrint(); // print board from h8 to a1
+	void DebugPrintPlayerPerspective(); // print board from a1 to h8
+	void PlacePiece(Coordinate* coord, BasePiece* piece); // Place "piece" on "coord"
 	void RemovePiece(Coordinate* coord);
 	size_t& GetSize();
-	Cell& GetCell(int id);
-	Cell& GetCell(int col, int row);
-	bool IsEmpty();
+	Cell& GetCell(int id); // get cell by id: id(0) -> A1, id(9) -> B2, id(63) -> H8 ...
+	Cell& GetCell(int col, int row); // get cell by position: Position(0, 0) -> A1, Position(1, 1) -> B2, Position(7,7) -> H8 ...
+	bool IsEmpty(); // checks if there are pieces on the board
 	
 private:
-	size_t _size = 0;
-	Cell* _cells = nullptr;
+	size_t _size = 0; // size of board
+	Cell* _cells = nullptr; // board's cells
 };
