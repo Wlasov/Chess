5b6cbe5cb96fbab4ac4e893b8abb1fec761a7b12
Nikolay Vlasov <93740526+Wlasov@users.noreply.github.com>
2023-05-25 19:41:29 +0300

    Update BasePiece.h

--- srcUPD/BasePiece.h
+++ srcUPD/BasePiece.h
@@ -10,10 +10,11 @@ public:
 	virtual ~BasePiece() {};
 	friend std::ostream& operator << (std::ostream& os, const BasePiece& piece);
 	
+	// Getters
 	EColor& GetColor();
 	std::string& GetSign();
 	
 private:
-	EColor _color = EColor::NoColor;
-	std::string _sign = "";
+	EColor _color = EColor::NoColor; //color of the piece
+	std::string _sign = ""; //the sign that is displayed in the console
 };
