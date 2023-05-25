787161143c04c79ea15c5e2a3e277954f0813423
Nikolay Vlasov <93740526+Wlasov@users.noreply.github.com>
2023-05-25 19:47:22 +0300

    Update Cell.h

--- srcUPD/Cell.h
+++ srcUPD/Cell.h
@@ -9,7 +9,7 @@ public:
 	Cell();
 	Cell(Coordinate* coord, EColor color, BasePiece* piece = nullptr);
 	~Cell() {};
-	bool IsEmpty() const;
+	bool IsEmpty() const; // checks if there is a piecee on this cell
 	Cell(const Cell& cell);
 	Cell& operator=(const Cell& other);
 	friend std::ostream& operator << (std::ostream& os, const Cell& cell);
@@ -17,7 +17,7 @@ public:
 	BasePiece* GetPiece();
 	
 private:
-	Coordinate* _coordinate = nullptr;
-	EColor _color = EColor::NoColor;
-	BasePiece* _currentPiece = nullptr;
+	Coordinate* _coordinate = nullptr; // coordinate of cell ( Coordinate(0, 0) means A1, Coordinate(7, 7) means H8...
+	EColor _color = EColor::NoColor; // Color of the cell
+	BasePiece* _currentPiece = nullptr; // the figure that stands on this cell
 };
