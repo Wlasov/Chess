81946cacd6e155c99436ebc2f7bbfb44ad7699e4
Nikolay Vlasov <93740526+Wlasov@users.noreply.github.com>
2023-05-25 19:56:20 +0300

    Update Queens.h

--- srcUPD/Queens.h
+++ srcUPD/Queens.h
@@ -10,12 +10,12 @@ public:
 	void MovePiece(const std::string& from, const std::string& to) override;
 	~Queens();
 	
-	void FindAllSolutions(int line);
-	void PrintAllSolutions();
+	void FindAllSolutions(int line); // finds all solutions for problem N queens for N board.
+	void PrintAllSolutions(); // prints solutions.
 	
 private:
-	Board* _solutions = nullptr;
-	Board* _board = nullptr;
-	size_t _solutionsCount = 0;
-	bool TryPlace(int, int);
+	Board* _solutions = nullptr; // an array of boards containing solutions to the n-queens problem.
+	Board* _board = nullptr; // board 0_0
+	size_t _solutionsCount = 0; // total solutuins count. for 8x8 board - 92.
+	bool TryPlace(int, int); // trying place Queen on Coordinate(x, y);
 };
